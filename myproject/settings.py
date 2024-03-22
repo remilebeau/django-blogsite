5fea7a17d93d24160584aad6a5ea02a7fd665adf
remilebeau <remilebeau90@gmail.com>
2024-03-22 12:20:23 -0400

    set Debug to false

--- myproject/settings.py
+++ myproject/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = "django-insecure-l^n))_ej&!^@3uz+h0ssh50pg3rdsrt3y-lx3!^-5)8zft*#3u"
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ["localhost", "127.0.0.1", "django-blogsite.onrender.com"]
 
